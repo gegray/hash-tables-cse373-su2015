b262736c0ebed818d4d2d3a13694923a9581c589
Austin Meyers <meyersaustin22@gmail.com>
2015-08-02 18:27:14 -0700

    Updated comments

--- src/shake_n_bacon/HashTable_SC.java
+++ src/shake_n_bacon/HashTable_SC.java
@@ -8,31 +8,10 @@ import providedCode.*;
  * @studentID 1463717, 1228316
  * @email gegray@uw.edu, arm38@uw.edu
  * 
- *        TODO: Replace this comment with your own as appropriate.
- * 
- *        1. You may implement HashTable with separate chaining discussed in
- *        class; the only restriction is that it should not restrict the size of
- *        the input domain (i.e., it must accept any key) or the number of
- *        inputs (i.e., it must grow as necessary).
- * 
- *        2. Your HashTable should rehash as appropriate (use load factor as
- *        shown in the class).
- * 
- *        3. To use your HashTable for WordCount, you will need to be able to
- *        hash strings. Implement your own hashing strategy using charAt and
- *        length. Do NOT use Java's hashCode method.
- * 
- *        4. HashTable should be able to grow at least up to 200,000. We are not
- *        going to test input size over 200,000 so you can stop resizing there
- *        (of course, you can make it grow even larger but it is not necessary).
- * 
- *        5. We suggest you to hard code the prime numbers. You can use this
- *        list: http://primes.utm.edu/lists/small/100000.txt NOTE: Make sure you
- *        only hard code the prime numbers that are going to be used. Do NOT
- *        copy the whole list!
- * 
- *        TODO: Develop appropriate tests for your HashTable.
- */
+**/
+
+//Creates a hash table that uses separate chaining to implement
+//the data structure
 public class HashTable_SC extends DataCounter {
 	//Declare global fields
 	private int size;
@@ -149,13 +128,13 @@ public class HashTable_SC extends DataCounter {
 					curr = data[index];
 					tempIndex = index;
 				}
-				DataCount data = curr.data;
+				DataCount val = curr.data;
 				curr = curr.next;
 				if (curr == null) {
 					index++;
 				}
 				tempSize--;			
-				return data;
+				return val;
 			}	
 			
 			//Method that returns true if the node has a child,
